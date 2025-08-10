ee3e341dda22895bf5e5dfecca5418fe4e1b128e
aveeeeen <taichimatsumoto360@gmail.com>
2025-08-10 21:59:17 +0900

    fixed begin song logic

--- src/components/layout/BeginPerformance.tsx
+++ src/components/layout/BeginPerformance.tsx
@@ -7,16 +7,10 @@ import {
 export const BeginPerformance = ({songId}: SongTitleOverlayProps) => {
   return (
     <>
-<<<<<<< HEAD
       <FrameLayer/>
       <SongTitleOverlay
         songId={songId}
       />
-=======
-      <FrameLayer />
-
-      <SongTitleOverlay songId={songId} />
->>>>>>> e91785a6bc2413a22127b81684f7a290962a4c02
     </>
   );
 };
